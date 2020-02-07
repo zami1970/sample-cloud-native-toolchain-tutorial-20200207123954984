3986f485b0260211616c8e74ff4adf4ebeb4b6d8
zami1970 <60775259+zami1970@users.noreply.github.com>
2020-02-07 14:28:12 +0100

    Update desireds.js

--- desireds.js
+++ desireds.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-  chrome: {browserName: 'chrome'},
-  //firefox: {browserName: 'firefox'},
+  //chrome: {browserName: 'chrome'},
+  firefox: {browserName: 'firefox'},
   //explorer: {browserName: 'internet explorer'}
 };
